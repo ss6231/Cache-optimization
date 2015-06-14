9bd2ee2f340e2342e198273a5b8f7070333bee47
Sana Sheikh <Ss6231@nyu.edu>
2015-06-14 12:45:35 -0400

    Update lvl1.c

--- lvl1.c
+++ lvl1.c
@@ -24,8 +24,11 @@ void level_5();
 /***********************************************/
 
 
-// For this level, I swapped the outer loop with the inner loop, so that the 2D array is being traversed in row-major order, which is exactly how the array is laid out in memory. The new function exhibits better spatial locality using a stride-1 reference pattern. Before the improvement, there was a stride-N reference pattern that resulted in a higher number of cache misses because the array elements were not being accessed consecutively and the  next memory address stored in the cache (which is organized in row-major order) is not
-// present, which  significantly slows down the program.
+// For this level, I swapped the outer loop with the inner loop, so that the 2D array is being traversed in row-major order, 
+//which is exactly how the array is laid out in memory. The new function exhibits better spatial locality using a stride-1 
+//reference pattern. Before the improvement, there was a stride-N reference pattern that resulted in a higher number of cache
+//misses because the array elements were not being accessed consecutively and the  next memory address stored in the cache 
+//(which is organized in row-major order) is not present, which  significantly slows down the program.
 
 
 int level_1()
@@ -46,7 +49,16 @@ int level_1()
 	
 }
 
-// This function summed up each column of B and put this summation into the corresponding diagonal of A. Before optimization, there was poor spatial locality because the inner loop was traversing in column-major order as opposed to row-major order, and because of this, the L1 cache never held the desired information, which resulted in cache misses. The general idea behind my optimization is to traverse B in row-major order, but update the diagonals of array A whenever it comes across the corresponding index of the inner loop. For example, my inner loop update is: A[j][j] += B[i][j] - if the current diagonal being updated is A[2][2], then this will be updated whenever j = 2, or in other words, whenever we are in the 2nd column. My update has a stride-1 reference pattern to exhibit good spatial locality. I decided to initialize the diagonals of A to 0 separately, because I needed to update the diagonal in such a way that adds the appropriate column value of B to the previously stored value in the corresponding diagonal of A so that I actually keep a running sum of the columns of B. If I kept the initialization of A as in the original function, the running sum of the columns would not be correct. 
+// This function summed up each column of B and put this summation into the corresponding diagonal of A. Before optimization, 
+//there was poor spatial locality because the inner loop was traversing in column-major order as opposed to row-major order, 
+//and because of this, the L1 cache never held the desired information, which resulted in cache misses. The general idea behind 
+//my optimization is to traverse B in row-major order, but update the diagonals of array A whenever it comes across the 
+//corresponding index of the inner loop. For example, my inner loop update is: A[j][j] += B[i][j] - if the current diagonal 
+//being updated is A[2][2], then this will be updated whenever j = 2, or in other words, whenever we are in the 2nd column. 
+//My update has a stride-1 reference pattern to exhibit good spatial locality. I decided to initialize the diagonals of A 
+//to 0 separately, because I needed to update the diagonal in such a way that adds the appropriate column value of B to the 
+//previously stored value in the corresponding diagonal of A so that I actually keep a running sum of the columns of B. 
+//If I kept the initialization of A as in the original function, the running sum of the columns would not be correct. 
 
 
 
@@ -76,7 +88,12 @@ void level_2()
 }
 
 
-// Originally, this function exhibited good temporal locality with respect to the variable temp since it is referenced frequently. However, this function had subpar spatial locality because it was sequencing through the 2D array through the array in column major order, which accounted for the large number of L1 misses. By instead sequencing row by row, the cache is able to exhibit exceptional spatial locality (since the cache will pick up data that is in proximity to the current memory address and produce more hits if sequencing in row major order) and temporal locality due to the role of the temp variable. So by swapping the outer and inner loops, the function is optimized and produces more cache hits. 
+// Originally, this function exhibited good temporal locality with respect to the variable temp since it is referenced 
+//frequently. However, this function had subpar spatial locality because it was sequencing through the 2D array through 
+//the array in column major order, which accounted for the large number of L1 misses. By instead sequencing row by row,
+//the cache is able to exhibit exceptional spatial locality (since the cache will pick up data that is in proximity to the 
+//current memory address and produce more hits if sequencing in row major order) and temporal locality due to the role of the 
+//temp variable. So by swapping the outer and inner loops, the function is optimized and produces more cache hits. 
 
 void level_3()
 {
@@ -98,7 +115,14 @@ void level_3()
 
 }
 
-// This function sorts a list of numbers using bubble sort. Not only does this form of sorting exhibit subpar complexity, it also results in numerous cache misses. Although it references numbers that are next to each other in the array (and are then stored in the cache), once the algorithm reaches the end, it starts back at the beginning and results in many more cache misses. We must utilize those numbers that are most recently stored in the cache (which are the latter numbers in the array) and somehow use those to continue sorting and prevent cache misses. I decided to implement a bidirectional bubble sort that would sequence through first from left to right, bubbling out the heaviest elements, and then sequence from right to left, bubbling out the lightest elements. This way, we are utilizing the elements that were most recently placed in the cache to allow us to decrease the number of cache misses. So in the end, the new sorting algorithm has good spatial locality. 
+// This function sorts a list of numbers using bubble sort. Not only does this form of sorting exhibit subpar complexity, it
+//also results in numerous cache misses. Although it references numbers that are next to each other in the array (and are 
+//then stored in the cache), once the algorithm reaches the end, it starts back at the beginning and results in many more cache 
+//misses. We must utilize those numbers that are most recently stored in the cache (which are the latter numbers in the array)
+//and somehow use those to continue sorting and prevent cache misses. I decided to implement a bidirectional bubble sort that
+//would sequence through first from left to right, bubbling out the heaviest elements, and then sequence from right to left, 
+//bubbling out the lightest elements. This way, we are utilizing the elements that were most recently placed in the cache to
+//allow us to decrease the number of cache misses. So in the end, the new sorting algorithm has good spatial locality. 
 
 void level_4() {
 	int i, j;
@@ -138,9 +162,12 @@ void level_4() {
 
 }
 
-// This function is multiplying elements that are not spatially related and putting the product into array C. A will produce many cache misses because we are jumping every 128th element starting at 0 and once we sequence all the way through, we move to the 1st element and then visit every 128th element from there. So all the elements that are placed in the cache that are nearby A[0] are not visited in the near future, resulting in many cache misses. Array C also exhibits this problem. Array B however does sequence through with a stride-1 pattern. By merely permuting the order of the for loops, we eliminate these problems significantly and result in an overall decrease in stride pattern due to better spatial locality.
-
-
+// This function is multiplying elements that are not spatially related and putting the product into array C. A will produce 
+//many cache misses because we are jumping every 128th element starting at 0 and once we sequence all the way through, we move 
+//to the 1st element and then visit every 128th element from there. So all the elements that are placed in the cache that are 
+//nearby A[0] are not visited in the near future, resulting in many cache misses. Array C also exhibits this problem. Array B
+//however does sequence through with a stride-1 pattern. By merely permuting the order of the for loops, we eliminate these 
+//problems significantly and result in an overall decrease in stride pattern due to better spatial locality.
 
 void level_5()
 {
